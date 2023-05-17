50276a05a05a9623175b02f95276941d23914502
Sujon Shekh <sj.sujon.shekh@gmail.com>
2023-05-17 11:29:33 +0600

    first-commit

---
+++ form.php
@@ -0,0 +1,33 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Day-14</title>
+    <link rel="stylesheet" href="css/bootstrap.css"/>
+    <link rel="stylesheet" href="css/all.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Our First Form</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, suscipit?</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <form action="">
+                    <label for="fname">First Name</label>
+                    <input type="text" class="form-control" name="fname" id="fname">
+                    <label for="lname">Last Name</label>
+                    <input type="text" class="form-control mb-3" name="lname" id="lname">
+                    <button type="submit" class="btn btn-outline-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+<script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
